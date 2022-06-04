8396f3c4e6b6677a5c40b2caaa7318cb03d9b192
Ognessa <vikapalihata@gmail.com>
2022-06-04 18:40:13 +0300

    update project 04.06.2022

--- app/src/main/java/carrira/elan/myapplication/ui/login/LoginFragment.kt
+++ app/src/main/java/carrira/elan/myapplication/ui/login/LoginFragment.kt
@@ -1,63 +1,39 @@
 package carrira.elan.myapplication.ui.login
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import carrira.elan.myapplication.R
+import carrira.elan.myapplication.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
-    private lateinit var tvLoginHint : TextView
     private val viewModel : LoginViewModel by viewModels()
+    lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding.loginViewModel = viewModel
 
-        val etEnterLogin : EditText = view.findViewById(R.id.et_enter_login)
-        val etEnterPass : EditText = view.findViewById(R.id.et_enter_pass)
-        val btnLogin : Button = view.findViewById(R.id.btn_login)
-        tvLoginHint = view.findViewById(R.id.tv_login_hint)
+        //TODO доробити data binding, не відображає текст
+        viewModel.message(requireContext()).observe(viewLifecycleOwner, Observer {
+            binding.tvLoginHint.text = it
+        })
 
-        btnLogin.setOnClickListener {
-            checkEnteredData(
-                etEnterLogin.text.toString(),
-                etEnterPass.text.toString())
+        binding.btnLogin.setOnClickListener{
+            viewModel.checkEnteredData(
+                binding.etEnterLogin.text.toString(),
+                binding.etEnterPass.text.toString())
         }
 
-        return view
-    }
-
-    @SuppressLint("UseCompatLoadingForDrawables")
-    fun checkEnteredData(login : String, pass : String){
-        val loginBool = viewModel.checkLogin(login)
-        val passBool = viewModel.checkPass(pass)
-        lateinit var message : String
-        tvLoginHint.visibility = View.VISIBLE
-
-        if(loginBool && passBool) {
-            message = requireContext().resources.getString(R.string.correct_login_and_pass)
-            tvLoginHint.background = resources.getDrawable(R.drawable.correct_login_background, null)
-        }
-        else{
-            if(!loginBool && passBool)
-                message = requireContext().resources.getString(R.string.incorrect_login)
-            else if(loginBool && !passBool)
-                message = requireContext().resources.getString(R.string.incorrect_pass)
-            else
-                message = requireContext().resources.getString(R.string.incorrect_login) + "\n\n" +
-                        requireContext().resources.getString(R.string.incorrect_pass)
-            tvLoginHint.background = resources.getDrawable(R.drawable.incorrect_login_background, null)
-        }
-
-        tvLoginHint.text = message
+        return binding.root
     }
 }
--- app/src/main/java/carrira/elan/myapplication/ui/login/LoginViewModel.kt
+++ app/src/main/java/carrira/elan/myapplication/ui/login/LoginViewModel.kt
@@ -1,16 +1,62 @@
 package carrira.elan.myapplication.ui.login
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import carrira.elan.myapplication.R
+
+enum class MessageValues {
+    ALL_CORRECT, INCORRECT_LOGIN, INCORRECT_PASSWORD, ALL_INCORRECT
+}
 
 class LoginViewModel : ViewModel() {
 
-    fun checkLogin(login : String) : Boolean{
+    private val _message = MutableLiveData<MessageValues>()
+
+    fun message(context : Context) : LiveData<String> {
+        val str = MutableLiveData<String>()
+        when(_message){
+            MutableLiveData(MessageValues.ALL_CORRECT) ->{
+                str.postValue(context.resources.getString(R.string.correct_login_and_pass))}
+            MutableLiveData(MessageValues.INCORRECT_LOGIN) ->{
+                str.postValue(context.resources.getString(R.string.incorrect_login))
+            }
+            MutableLiveData(MessageValues.INCORRECT_PASSWORD) ->{
+                str.postValue(context.resources.getString(R.string.incorrect_pass))
+            }
+            MutableLiveData(MessageValues.ALL_INCORRECT) ->{
+                str.postValue(context.resources.getString(R.string.incorrect_login) +
+                "\n\n" + context.resources.getString(R.string.incorrect_pass))
+            }
+        }
+        return str
+    }
+
+    fun checkEnteredData(login : String, pass : String){
+        val loginBool = checkLogin(login)
+        val passBool = checkPass(pass)
+
+        if(loginBool && passBool) {
+            _message.postValue(MessageValues.ALL_CORRECT)
+        }
+        else{
+            if(!loginBool && passBool)
+                _message.postValue(MessageValues.INCORRECT_LOGIN)
+            else if(loginBool && !passBool)
+                _message.postValue(MessageValues.INCORRECT_PASSWORD)
+            else
+                _message.postValue(MessageValues.ALL_INCORRECT)
+        }
+    }
+
+    private fun checkLogin(login : String) : Boolean{
         if(login.contains(" ") || login.isEmpty())
             return false
         return true
     }
 
-    fun checkPass(pass : String) : Boolean{
+    private fun checkPass(pass : String) : Boolean{
         val regexNum = Regex("[0-9]")
         val regexSmallLetters = Regex("[a-z]")
         val regexBigLetters = Regex("[A-Z]")
